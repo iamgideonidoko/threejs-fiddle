26d6ac4e9c937a3c693454ca76027fce6a78b535
Gideon Idoko <iamgideonidoko@gmail.com>
2022-10-11 09:42:46 +0100

    transformation in 3D space

--- src/World/components/cube.ts
+++ src/World/components/cube.ts
@@ -1,4 +1,4 @@
-import { BoxGeometry, Mesh, MeshStandardMaterial } from 'three';
+import { BoxGeometry, Mesh, MeshStandardMaterial, MathUtils } from 'three';
 
 const createCube = (): Mesh<any, MeshStandardMaterial> => {
   // create a geometry
@@ -10,7 +10,27 @@ const createCube = (): Mesh<any, MeshStandardMaterial> => {
   // create a Mesh containing the geometry and material
   const cube = new Mesh(geometry, material);
 
-  cube.rotation.set(-0.5, -0.1, 0.8);
+  // cube.rotation.set(-0.5, -0.1, 0.8);
+
+  cube.position.x = -0.5;
+  cube.position.y = -0.1;
+  cube.position.z = 1;
+
+  // equivalent to:
+  // cube.position.set(-0.5, -0.1, 1);
+
+  cube.scale.x = 1.25;
+  cube.scale.y = 0.25;
+  cube.scale.z = 0.5;
+
+  // equivalent to:
+  // cube.scale.set(1.25, 0.25, 0.5);
+
+  // to rotate using degrees, they must
+  // first be converted to radians
+  cube.rotation.x = MathUtils.degToRad(-60);
+  cube.rotation.y = MathUtils.degToRad(-45);
+  cube.rotation.z = MathUtils.degToRad(60);
 
   return cube;
 }
